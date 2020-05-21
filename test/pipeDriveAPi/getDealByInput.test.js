d15aa9ffc1a3336903d03199acc557f933291e29
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2020-05-21 15:41:46 -0300

    Add new test for mongobd atlas

--- test/pipeDriveAPi/getDealByInput.test.js
+++ test/pipeDriveAPi/getDealByInput.test.js
@@ -1,14 +1,14 @@
-import  getDealByInput  from '../../src/pipeDriveApi/getDealByInput'
 import chai from 'chai'
+import getDealByInput from '../../src/services/pipeDriveApi/getDealByInput'
 
 const expect = chai.expect
 
-describe('Get data from pipedrive api', () => {
+describe('#Get data from pipedrive api', () => {
   it('it should return deals with success = true', async () => {
     const input = []
-      input['staus'] = 'won'
+          input['status'] = 'won'
     
     const deals = await getDealByInput(input)
     expect(deals.success).to.be.true
-  })
+  }).timeout(3000)
 })
