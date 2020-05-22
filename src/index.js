933683163401df1b2f838c58f67e89b747e0bd99
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2020-05-22 05:21:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import { getProductsByDeal, getDealByRecents } from './services/pipeDriveApi/get
 import { dealsWon } from './utils/filters'
 import { fullTime } from './utils/utils'
 
-// const dias = '96050a68f95be39d0bde2fcfc2dec7af22d51d0f'
-// const parcela = 'a76029a413070c37c6246ee3159f89c0d25d292e'
-
 const getIdFromRecentsDeals = async () => {
   const input = []
   input['sinceTimestamp'] = fullTime 
@@ -25,19 +22,6 @@ const getIdFromRecentsDeals = async () => {
 }
 
 getIdFromRecentsDeals()
-// const testData = async () => {
-//   ' 
-
-//   const DealRecents =  await getDealByRecents(input)
-//   const result =  DealRecents.data.map((el) => { return { nome : el.data.org_name }})
-//   console.log(result)
-//   return result
-  
-// }
-
-// const getProducts = (id) => {
-//   return
-// }
 
 const input = [] 
     input['id'] = 9
