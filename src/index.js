58f09e0a91b72f52682a044283f1531732e73f3b
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2020-05-25 05:24:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { getProductsByDeal, getDealByRecents } from './services/pipeDriveApi/getDataFromPipeDrive'
-import { dealsWon } from './utils/filters'
-import { fullTime } from './utils/utils'
-
-const getIdFromRecentsDeals = async () => {
-  const input = []
-  input['sinceTimestamp'] = fullTime 
-  input['items'] = 'deal'
-
-  const DealsRecents = await getDealByRecents(input)
-  return DealsRecents.data.map((deal) => {
-    return { 
-      id: deal.data.id,
-      fornecedor: deal.data.org_name,
-      dias_pagamento: 0,
-      valor_parcela: 0,
-     }
-  })  
-}
-
-getIdFromRecentsDeals()
-
-const input = [] 
-    input['id'] = 9
-
-getProductsByDeal(input)
- .then(response => console.log(response))
-=======
 import getDataToSendBling from './services/pipeDriveApi/getDataToSendBling'
 import saveProductsToSendBling from './services/pipeDriveApi/saveProductsToSendBling'
 
@@ -41,4 +12,4 @@ const mainFunction = async () => {
 }
 
 mainFunction()
->>>>>>> development
+
