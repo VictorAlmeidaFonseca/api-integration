f97cb598c90cc1fb1c860873d30388275b871ae3
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2021-01-20 10:48:22 -0300

    Update sendProducts.js

--- src/services/blingApi/sendProducts.js
+++ src/services/blingApi/sendProducts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiKey = 'fe2fdeec547997667cc629cb4cb30925bf70875ccbb1c52ee095e460bc89603f0633d949'
+const apiKey = ''
 
 const Send = axios.create({
   baseURL: `"https://bling.com.br/Api/v2/pedido/json/"`,
