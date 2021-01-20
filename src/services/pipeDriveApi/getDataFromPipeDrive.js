0058642a48f114a3d9a604e9b9e7d60d06cbb9a9
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2021-01-20 10:48:52 -0300

    Update getDataFromPipeDrive.js

--- src/services/pipeDriveApi/getDataFromPipeDrive.js
+++ src/services/pipeDriveApi/getDataFromPipeDrive.js
@@ -2,7 +2,7 @@ import { Configuration,
          RecentsController,
          DealsController } from 'pipedrive'
 
-Configuration.apiToken = 'c2c46b8af3ee0e47f77462d49e8786040da99cad'
+Configuration.apiToken = ''
 
 const getDealByRecents = async (input) => {
   const searchDeals = RecentsController.getRecents(input)
